a26d8fe1079940c1acc7ee8ef9023db76198268b
n-adilla <adillanurdin@gmail.com>
2024-03-06 10:13:11 +0800

    Upload the project files, add packages, add gitignore

---
+++ lib/changes/links.dart
@@ -0,0 +1,38 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/112sSAW-p8JgXOpAco769c4xarpcA68tL/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/n-adilla';
+// Github Link
+const String facebook = 'https://facebook.com/arson.adilla/';
+// Github Link
+const String twitter = 'https://twitter.com/diruru_';
+// Github Link
+const String youtube =
+    'https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg';
+// Github Link
+const String linkedin = 'https://linkedin.com/in/adillanurdin';
+// Github Link
+const String buymeacoffee = 'https://www.buymeacoffee.com/Adillanb';
+// Github Link
+const String instagram = 'https://www.instagram.com/diruru_/';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/Adillanb",
+//   // "https://instagram.com/Adilla_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/Adilla78",
+//   "https://linkedin.com/in/Adillanb",
+//   "https://www.buymeacoffee.com/Adillanb", "https://github.com/Adillanb",
+// ];
+//
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+const String fiverr = 'https://www.fiverr.com/Adillanb';
+// Github Link
+const String upwork = 'https://www.upwork.com/freelancers/~015c46144016569e31';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=60108638963';
---
+++ lib/changes/strings.dart
@@ -0,0 +1,29 @@
+String hellotag = '''Hi there, Welcome to my portfolio site!  ''';
+
+String yourname = "I'm Adilla Nurdin,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " A Mobile Application Developer";
+String animationtxt2 = " An iOS Developer";
+String animationtxt3 = " An Android Developer";
+String animationtxt4 = " A Web Developer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "I am a passionate software developer who has been developing mobile apps using Flutter for over 3 years. Throughout my journey, I have gained experience in Java Android and JavaScript web development as well. I am driven by my enthusiasm for exploring emerging technologies and always eager to stay up-to-date with the latest advancements in the field. With my analytical mindset and practical problem-solving approach, I tackle challenges with determination and strive to see projects through to completion. Additionally, I am skilled in project management and adept at preparing user documentation to ensure seamless project execution.";
+//
+
+String portfolioSubHeading =
+    "Since the beginning of my journey as a software developer, I have worked/been working on a few application projects. Here are some of them:";
+const String aboutMeHeadline =
+    'I\'m Adilla Nurdin, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
